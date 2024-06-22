caf2e3186e2e2de8c82ff4826368a35873817b52
hezulong1 <h2108b@163.com>
2024-06-22 22:46:50 +0800

    chore: 适配 lit 配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,36 @@
+const pkg = require('./package.json');
+
+const restricted = [
+  {
+    name: 'vue',
+    importNames: ['MaybeRefOrGetter', 'toValue'],
+  },
+];
+
+const dependencies = Object.keys(pkg.dependencies || {})
+  .filter(pattern => ['vue', 'lit', '@vueuse/core'].includes(pattern))
+  .map(pattern => ({ pattern, group: 'external', position: 'after' }));
+
 module.exports = {
   root: true,
-  extends: 'h21',
+  extends: [
+    'h21',
+    'plugin:lit/recommended',
+  ],
   rules: {
+    'no-restricted-imports': [2, { paths: restricted }],
     'import/order': [2, {
       groups: ['type', 'builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+      pathGroups: [
+        { pattern: 'vitest', group: 'builtin', position: 'after' },
+        { pattern: 'vue', group: 'external', position: 'before' },
+        { pattern: 'lit', group: 'external', position: 'before' },
+        { pattern: '@vue/**', group: 'external', position: 'before' },
+        { pattern: '@vueuse/**', group: 'external', position: 'before' },
+        ...dependencies,
+        { pattern: '@/**', group: 'internal', position: 'before' },
+      ],
+      pathGroupsExcludedImportTypes: ['type'],
     }],
   },
   overrides: [
